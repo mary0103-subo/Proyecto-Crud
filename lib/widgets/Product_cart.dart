90a05c0f77cf0170cb068ccca919d31dd4ded9e5
Mary Carmen Huamani Martinez <201615146h@utea.edu.pe>
2021-08-15 11:32:15 -0500

    Widget parte 2

--- lib/widgets/Product_cart.dart
+++ lib/widgets/Product_cart.dart
@@ -19,8 +19,18 @@ class ProduCart extends StatelessWidget {
 
             _BackgroundImage(),
 
-             _ProductDetails()
-
+             _ProductDetails(),
+             
+             Positioned(
+               top: 0,
+               right: 0,
+               child: _PriceTag()
+               ),
+               Positioned(
+               top: 0,
+               left: 0,
+               child: _NotAvailabke()
+               )
           ],
         ),
    
@@ -42,24 +52,93 @@ class ProduCart extends StatelessWidget {
   
 }
 
+class _NotAvailabke extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: FittedBox(
+        fit: BoxFit.contain,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          child: Text(
+            'No disponible',
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
+        ),
+      ),
+      width: 100,
+      height: 70,
+      decoration: BoxDecoration(
+        color: Colors.yellow[800],
+        borderRadius: BorderRadius.only( topLeft: Radius.circular(25),bottomRight: Radius.circular(25))
+        ),
+    );
+  }
+}
+
+class _PriceTag extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: FittedBox(
+        fit: BoxFit.contain,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          child: Text('\$100.99',style: TextStyle(color: Colors.white, fontSize: 20),)),
+      ),
+      width: 100,
+      height: 70,
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        color: Colors.indigo,
+        borderRadius: BorderRadius.only(topRight: Radius.circular(25),bottomLeft: Radius.circular(25))
+      ),
+    );
+  }
+}
+
 class _ProductDetails extends StatelessWidget {
   
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: 70,
-      color: Colors.red,
+    return Padding(
+      padding: EdgeInsets.only( right: 50 ),
+      child: Container(
+        padding:EdgeInsets.symmetric(horizontal: 20,vertical: 10) ,
+        width: double.infinity,
+        height: 70,
+       decoration:  _buildBoxDecoration(), 
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Disco duro G',
+            style: TextStyle(fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold),
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+            ),
+            Text('Id del disco duro',
+            style: TextStyle(fontSize: 15, color: Colors.white),
+            
+            )
+          ],
+        ),
+      ),
     );
   }
+
+  BoxDecoration _buildBoxDecoration() => BoxDecoration(
+   color: Colors.indigo,
+   borderRadius: BorderRadius.only(bottomLeft: Radius.circular(25),topRight: Radius.circular(25))
+  );
 }
 
 class _BackgroundImage extends StatelessWidget {
   
   @override
-  Widget build(BuildContext context) {
-    
+  Widget build(BuildContext context) {  
     return ClipRRect(
       borderRadius: BorderRadius.circular(25),
       child: Container(
