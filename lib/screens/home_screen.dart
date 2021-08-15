108782055eb719ea92dee61ba02e0a351883f150
Mary Carmen Huamani Martinez <201615146h@utea.edu.pe>
2021-08-14 23:18:00 -0500

    pantalla de productos

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:productos_app/widgets/widgets.dart';
+
+
+class HomeScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+     appBar: AppBar(
+       title: Text('Producto'),
+     ),
+     body: ListView.builder
+     (itemCount: 10,
+       itemBuilder:( BuildContext context,int index) => ProduCart()
+     ),
+     floatingActionButton:FloatingActionButton(
+       child: Icon(Icons.add),
+       onPressed: () {},
+     ) ,
+   );
+  }
+}
---
+++ lib/widgets/widgets.dart
@@ -0,0 +1,12 @@
+
+
+
+export 'package:productos_app/widgets/auth_background.dart';
+export 'package:productos_app/widgets/card_container.dart';
+export 'package:productos_app/widgets/Product_cart.dart'; 
+
+
+
+
+
+
