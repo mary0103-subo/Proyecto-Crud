e299f586ff4a1ab166228126ca2c47e21ad5c5c7
Mary Carmen Huamani Martinez <201615146h@utea.edu.pe>
2021-08-15 00:51:53 -0500

    widget

--- lib/widgets/Product_cart.dart
+++ lib/widgets/Product_cart.dart
@@ -12,7 +12,44 @@ class ProduCart extends StatelessWidget {
         margin: EdgeInsets.only(top: 30,bottom: 50),
         width: double.infinity,
         height: 400,
-        color: Colors.red,
+        decoration:  _cardBorders(),
+        child: Stack(
+          children: [
+
+            _BackgroundImage()
+          ],
+        ),
+   
+      ),
+    );
+  }
+
+  BoxDecoration _cardBorders() => BoxDecoration(
+    color: Colors.white,
+    borderRadius: BorderRadius.circular(25),
+    boxShadow: [
+      BoxShadow(
+        color: Colors.black12,
+        offset: Offset(0,7),
+        blurRadius: 10
+      )
+    ]
+  );
+  
+}
+
+class _BackgroundImage extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    
+    return Container(
+      width: double.infinity,
+      height: 400,
+      color: Colors.red,
+      child: FadeInImage(
+        placeholder: AssetImage('assets/jar-loading.gif'),
+        image: NetworkImage('https://via.placehoder.com/400x300/f6f6f6'),
       ),
     );
   }
